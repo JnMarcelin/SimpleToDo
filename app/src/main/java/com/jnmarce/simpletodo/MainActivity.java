703240897d2c726760cef72d9d3ffd34502e6f57
Jean Marcelin Gelin <jeanmarcelingelin@gmail.com>
2018-11-09 12:20:13 -1200

    Initial commit

---
+++ app/src/main/java/com/jnmarce/simpletodo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.jnmarce.simpletodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // obtain a reference to the LisView created with the layout
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        // initialise the item list
+        items = new ArrayList<>();
+        //initialise the adapter using the items list
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        // wire the adapter to the view
+        lvItems.setAdapter(itemsAdapter);
+
+        //add some mock items to the list
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+
+        // setup the listener on creation
+        setupListViewListener();
+
+    }
+
+    private void setupListViewListener() {
+        // set the ListView's itemLongClickListener
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // remove the item in the list at the index given by position
+                items.remove(position);
+                // notify the adapter that the underlying data set changed
+                itemsAdapter.notifyDataSetChanged();
+                // return true to tell the framework that the long click was consumed
+                return true;
+            }
+        });
+    }
+
+    public void onAddItem(View v) {
+        // obtain a reference to the EditText created with the layout
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        // grab the EditText's content as a String
+        String itemText = etNewItem.getText().toString();
+        // add the item to the list via the adapter
+        itemsAdapter.add(itemText);
+        // clear the EditText by setting it to an empty String
+        etNewItem.setText("");
+    }
+    
+}
